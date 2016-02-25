0bb6b62e34f0f718809ddefcc84ea8cb8fd8b8e6
Donald Kirk <B2160104@ben.edu>
2016-02-24 22:35:27 -0600

    Made current user redirect to their own profile page by their id.

--- src/main/java/edu/ben/template/controller/HomeController.java
+++ src/main/java/edu/ben/template/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -252,6 +253,7 @@ public class HomeController extends BaseController {
 				User register = new User();
 
 				register.setEmail(benEmail);
+				register.setPersonalEmail(personalEmail);
 				register.setFirstName(firstName);
 				register.setLastName(lastName);
 
@@ -609,30 +611,16 @@ public class HomeController extends BaseController {
 	 *            is being passed in.
 	 * @return the profile page that belongs to the user.
 	 */
-	@RequestMapping(value = "/userProfile", method = RequestMethod.GET)
-	// public String userProfile(Model model, @RequestParam("name") String name,
-	// @RequestParam("bio") String biography,
-	// @RequestParam("occupation") String occupation,
-	// @RequestParam("graduation") String graduation,
-	// @RequestParam("workInterest") String workInterest,
-	// @RequestParam("experience") String experience) {
-	public String userProfile(Model model) {
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public String userProfile(Model model, @PathVariable Long id) {
 
-		// try {
-		//
-		// ArrayList<User> user = new ArrayList<User>();
-		// users = getJdbcTypeDao().findAll();
-		//
-		// model.addAttribute("users", users);
-		//
-		// } catch (Exception e) {
-		// }
-		//
+		User userId = getUserDao().getObjectById(id);
+		model.addAttribute("userId", userId);
 
 		return "userProfile";
 	}
 
-	@RequestMapping(value = "/userProfile", method = RequestMethod.POST)
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.POST)
 	public String userProfileUpload(Model model, @RequestParam CommonsMultipartFile[] fileUpload) throws Exception {
 
 		// if (fileUpload != null && fileUpload.length > 0) {

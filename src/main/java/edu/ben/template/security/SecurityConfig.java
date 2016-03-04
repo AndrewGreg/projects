324a98a641e20629cf83e817b5d31034c541e762
Jose Dieck <jdieck@cs.ben.edu>
2016-03-04 16:48:50 -0600

    Modified the index page to the new one...added the template files to the directory

--- src/main/java/edu/ben/template/security/SecurityConfig.java
+++ src/main/java/edu/ben/template/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
 				.disable()
-				.authorizeRequests().antMatchers("/index").permitAll().antMatchers("/register").permitAll()
+				.authorizeRequests().antMatchers("/index").permitAll().antMatchers("/register").permitAll().antMatchers("/").permitAll()
 				/* TODO Add any additional open links here */
 				.anyRequest().authenticated()
 				.and()

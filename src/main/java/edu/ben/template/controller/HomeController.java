f0064ef871b04f95d1efa6667f87820c93ebebbd
Donald Kirk <B2160104@ben.edu>
2016-03-11 23:11:23 -0600

    Changed Sort method to sort by first name.

--- src/main/java/edu/ben/template/controller/HomeController.java
+++ src/main/java/edu/ben/template/controller/HomeController.java
@@ -56,7 +56,7 @@ public class HomeController extends BaseController {
 		Collections.sort(user, new Comparator<User>() {
 			@Override
 			public int compare(User o1, User o2) {
-				return o1.getLastName().compareTo(o2.getLastName());
+				return o1.getFirstName().compareTo(o2.getFirstName());
 			}
 		});
 	}

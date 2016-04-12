0bbdf8b53b0b1443c9c1a8fab328b8b8c9b85e65
Donald Kirk <B2160104@ben.edu>
2016-04-12 00:08:02 -0500

    Added better dummy data for event and jobs.

--- docs/Dummy Data Script.sql
+++ docs/Dummy Data Script.sql
@@ -41,19 +41,28 @@ insert into user (id, email,password,salt,first_name, last_name, role, title_id)
 insert into user (id, email,password,salt,first_name, last_name, role, title_id) values (32,'admin@ben.edu', 'a13d4b8b59ff5768129bc50083ac6ecc16078be5bdf0d462577399d6a10b2a73a084bc1cb4930e72', 'null', 'Alice', 'Admin', 4, 2);
 
 
-
-
-
-INSERT INTO `event` (`id`, `name`, `date`, `description`, `user_id`, `hidden`)
+INSERT INTO `job` (`id`, `name`, `description`, `company`, `user_id`, `hidden`, `end_date`, `start_date`, `reference`, `public`, `hours_id`, `salary`, `start_wage`, `end_wage`, `start_salary`, `end_salary`, `link`, `location`)
 VALUES
-	(1, 'job fair', '2016-02-26', 'Job fair stuff', 1, 0),
-	(2, 'Sprint Review', '2016-02-26', 'go over the sprint that we did.', 31, 0),
-	(3, 'Sprint Review', '2016-02-26', 'Going over the sprint.', 31, 0),
-	(4, 'Example 2', '2016-03-01', 'This is a test.', 31, 0),
-	(5, 'Example 5', '2016-03-01', 'This is a test again.', 31, 0),
-	(6, 'Example 3', '2016-03-01', 'This is a test and again.', 31, 0),
-	(7, 'Test', '2016-03-01', 'This is a Test', 31, 0),
-	(8, 'Review Sprint 3', '2016-03-02', 'Go over this at 3pm', 31, 0);
+	(1, 'Software Developer', 'You are always coding forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Microsoft', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 100000, 150000, NULL, 'California'),
+	(2, 'Web Development (Front End)', 'You are always developing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Google Inc', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 100000, 150000, NULL, 'Illinois'),
+    (3, 'Sql Developer', 'You are always developing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Geek Squad', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 210000, 305000, NULL, 'Iowa'),
+	(4, 'I.T Support', 'You are always supporting forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'BP', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 5000, 10000, NULL, 'Maine'),
+(5, 'Technical Support Specialist', 'You are always helping forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'McDonalds', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 1000, 2000, NULL, 'Florida'),
+(6, 'Web Development (Back End)', 'You are always developing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Best Buy', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 60000, 70000, NULL, 'New York'),
+(7, 'Java Tester', 'You are always testing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Target', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 50000, 65000, NULL, 'South Carolina'),
+(8, 'Ruby Developer', 'You are always developing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Government', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 40000, 150000, NULL, 'Mexico'),
+(9, 'Web Designer', 'You are always designing forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever and forever.', 'Apple', 31, 0, NULL, NULL, NULL, 1, 2, 0, 0, 0, 100000, 150000, NULL, 'Rhode Island');
+
+
+INSERT INTO `event` (`id`, `name`, `date`, `description`, `user_id`, `hidden`, `start_time`, `end_time`, `public`, `longitude`, `latitude`, `role`, `reference`, `location`)
+VALUES
+	(1, 'Sprint 6 Meeting', '2016-04-13', 'Go over the SOW over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over over and over .', 31, 0, '12:00', '14:00', 1, 0, 0, 0, NULL, 'Benedictine University'),
+	(2, 'Graduation', '2016-05-14', 'Join us in celebrating the class of 2016.', 31, 0, '09:00', '11:00', 1, 0, 0, 0, NULL, 'Benedictine University'),
+	(3, 'Sprint Review Meeting', '2016-04-12', 'We are meeting to review our sprint 6 meeting.', 31, 0, '14:00', '15:00', 1, 0, 0, 0, NULL, 'Benedictine University'),
+	(4, 'The Gettysburg Address', '2018-05-14', 'On June 1, 1865, Senator Charles Sumner referred to the most famous speech ever given by President Abraham Lincoln. In his eulogy on the slain president, he called the Gettysburg Address a "monumental act." He said Lincoln was mistaken that "the world will little note, nor long remember what we say here." Rather, the Bostonian remarked, "The world noted at once what he said, and will never cease to remember it. The battle itself was less important than the speech."', 31, 0, '10:00', '13:00', 1, 0, 0, 0, NULL, 'Gettysburg Pennsylvania'),
+	(5, '4th of July', '2016-07-04', 'We celebrate American Independence Day on the Fourth of July every year. We think of July 4, 1776, as a day that represents the Declaration of Independence and the birth of the United States of America as an independent nation.', 31, 0, '01:00', '23:00', 1, 0, 0, 0, NULL, 'Earth'),
+    (6, 'Founding of Benedictine', '2020-05-14', 'Join us in celebrating the finding of Benedictine University.', 31, 0, '09:00', '11:00', 1, 0, 0, 0, NULL, 'Benedictine University');
+    
 
 INSERT INTO `interest` (`id`, `name`, `hidden`)
 VALUES
